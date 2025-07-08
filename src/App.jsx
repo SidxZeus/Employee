52d2f213b0c867e05e4259a30c488d70342cd966
Siddhu <siddhukamble7385@gmail.com>
2025-07-08 15:21:48 +0530

    Implement authentication logic and local storage utilities; add AuthContext and TaskContext components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import Login from './components/Auth/Login'
-import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
-import AdminDashboard from './components/Dashboard/AdminDashboard'
+import React, { useEffect, useState } from "react";
+import Login from "./components/Auth/Login";
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+import AdminDashboard from "./components/Dashboard/AdminDashboard";
+import { getLocalStorage, setLocalStorage } from "./utils/LocalStorage";
 
 const App = () => {
-  return (
-   <>
-    {/* <Login /> */}
-    {/* {<EmployeeDashboard/>} */}
-    <AdminDashboard/>
-   </>
-  )
-}
+  const [user, setUser] = useState(null);
+  const handleLogin = (email, password) => {
+    if (email == "admin@me.com" && password == "123") {
+      console.log("This is admin");
+    } else if (email == "user@me.com" && password == "123") {
+      console.log("This is user");
+    } 
+    else {
+      alert("Invalid credentials, please try again.");
+    }
+  };
+  handleLogin("user@me.com", 123);
+  // Example usage (remove or adjust as needed)
+  // handleLogin('admin@me.com', '123')
 
-export default App
+  // useEffect(() => {
+  //   // setLocalStorage()
+  //   getLocalStorage()
+  // },[])
+
+  return (
+    <>
+      {!user ? <Login handleLogin={handleLogin} /> : ""}
+      {/* {<EmployeeDashboard/>} */}
+      {/* <AdminDashboard/> */}
+    </>
+  );
+};
+
+export default App;
